29aabf2d139e15ef3b35f0710881a5f8ca923a82
Łukasz <witkowski553@gmail.com>
2020-11-13 20:02:23 +0100

    add getPopularMovies function to repository

--- app/src/main/java/com/example/android/popularmovies/MainRepository.kt
+++ app/src/main/java/com/example/android/popularmovies/MainRepository.kt
@@ -1,9 +1,20 @@
 package com.example.android.popularmovies
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.android.popularmovies.models.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MainRepository(private val application: Application) {
 
     //private val database
+
+
+    suspend fun getPopularMovies(): LiveData<Movie>{
+        return withContext(Dispatchers.IO){
+
+        }
+    }
 
 }
