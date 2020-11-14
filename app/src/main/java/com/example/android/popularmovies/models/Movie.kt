a9730ec24d5a66ae2eb0537fa1767c278b3349c3
Łukasz <witkowski553@gmail.com>
2020-11-14 14:51:18 +0100

    modify for json response

--- app/src/main/java/com/example/android/popularmovies/models/Movie.kt
+++ app/src/main/java/com/example/android/popularmovies/models/Movie.kt
@@ -1,7 +1,9 @@
 package com.example.android.popularmovies.models
 
 import android.net.Uri
+import com.squareup.moshi.Json
 
+/*
 data class Movie(
         var id: Int = 0,
         var originalTitle:String = "",
@@ -13,4 +15,29 @@ data class Movie(
         var videoUrls: Array<String> = emptyArray<String>(),
         var reviews: Array<Review> = emptyArray<Review>(),
         var isFavourite: Boolean = false
+)*/
+
+data class Movie (
+        var popularity: Float,
+        @Json(name="vote_count")
+        var voteCount: Int,
+        var video: Boolean,
+        @Json(name="poster_path")
+        var posterPath: String,
+        var id: Long,
+        var adult: Boolean,
+        @Json(name="backdrop_path")
+        var backdropPath: String,
+        @Json(name="original_language")
+        var originalLanguage: String,
+        @Json(name="original_title")
+        var originalTitle: String,
+        @Json(name="genre_ids")
+        var genreIds: List<Long>,
+        var title: String,
+        @Json(name="vote_average")
+        var voteAverage: Float,
+        var overview: String,
+        @Json(name="release_date")
+        var releaseDate: String
 )
