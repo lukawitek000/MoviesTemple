ae9b2aa3deded25d3aaf83d662f5f1a66a2d38c5
Łukasz <witkowski553@gmail.com>
2020-12-11 17:07:31 +0100

    set up hilt dependency injection framework

---
+++ app/src/main/java/com/lukasz/witkowski/android/moviestemple/MoviesTempleApplication.kt
@@ -0,0 +1,7 @@
+package com.lukasz.witkowski.android.moviestemple
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MoviesTempleApplication: Application()
