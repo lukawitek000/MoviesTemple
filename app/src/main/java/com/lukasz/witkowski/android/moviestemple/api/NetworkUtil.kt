14dc127c3141980e262d371b9155e5bda2b66ba7
Łukasz Witkowski <58557988+lukawitek000@users.noreply.github.com>
2021-02-14 10:12:19 +0100

    delete api key

--- app/src/main/java/com/lukasz/witkowski/android/moviestemple/api/NetworkUtil.kt
+++ app/src/main/java/com/lukasz/witkowski/android/moviestemple/api/NetworkUtil.kt
@@ -5,7 +5,7 @@ package com.lukasz.witkowski.android.moviestemple.api
 const val IMAGE_WIDTH = 500
 const val URL_ADDRESS = "https://api.themoviedb.org"
 const val API_KEY = "api_key"
-const val api_key = "3b623a17f57eb4da612b3871d3f78ced"
+const val api_key = "" // here goes your API KEY from TMDB Api
 const val POSTER_BASE_URI = "http://image.tmdb.org/t/p/w500"
 const val VIDEO_BASE_URI = "https://www.youtube.com/watch?v="
 const val PERSON_BASE_URI = "https://www.themoviedb.org/person/"
